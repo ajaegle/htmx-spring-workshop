8c7af74a86a065864893f34a47bfe867cd0c3e80
Thomas Schuehly <thomas.schuehly@outlook.com>
2024-05-12 18:01:46 +0200

    Checkpoint 3

--- lab-4/src/main/java/de/tschuehly/easy/spring/auth/user/management/UserManagement.java
+++ lab-4/src/main/java/de/tschuehly/easy/spring/auth/user/management/UserManagement.java
@@ -6,7 +6,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
-@Order(1)
 public class UserManagement implements Page {
 
   @Override
