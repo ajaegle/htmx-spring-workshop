f8abb6fec745c37f510c0bf010aa66c8e5991496
Thomas Schuehly <thomas.schuehly@outlook.com>
2024-05-12 14:44:20 +0200

    Checkpoint 1

--- lab-3/src/main/java/de/tschuehly/easy/spring/auth/group/GroupController.java
+++ lab-3/src/main/java/de/tschuehly/easy/spring/auth/group/GroupController.java
@@ -1,12 +1,22 @@
 package de.tschuehly.easy.spring.auth.group;
 
+import de.tschuehly.easy.spring.auth.group.management.GroupManagement;
+import de.tschuehly.spring.viewcomponent.jte.ViewContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GroupController {
   private final GroupService groupService;
+  private final GroupManagement groupManagement;
 
-  public GroupController(GroupService groupService) {
+  public GroupController(GroupService groupService, GroupManagement groupManagement) {
     this.groupService = groupService;
+    this.groupManagement = groupManagement;
+  }
+
+  @GetMapping("/groupManagement")
+  public ViewContext groupManagement(){
+    return groupManagement.render();
   }
 }
