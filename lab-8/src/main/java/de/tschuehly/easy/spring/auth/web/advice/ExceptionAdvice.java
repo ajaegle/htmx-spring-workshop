2ef3197ce78ec6aa4ca7ad49823ce08f2713c024
Thomas Schuehly <thomas.schuehly@outlook.com>
2024-05-13 10:14:27 +0200

    lab-8 checkpoint 1

---
+++ lab-8/src/main/java/de/tschuehly/easy/spring/auth/web/advice/ExceptionAdvice.java
@@ -0,0 +1,27 @@
+package de.tschuehly.easy.spring.auth.web.advice;
+
+import de.tschuehly.easy.spring.auth.htmx.HtmxUtil;
+import de.tschuehly.easy.spring.auth.web.exception.InfoException;
+import de.tschuehly.easy.spring.auth.web.layout.LayoutComponent;
+import de.tschuehly.easy.spring.auth.web.message.MessageComponent;
+import de.tschuehly.spring.viewcomponent.jte.ViewContext;
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxSwapType;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionAdvice {
+
+  private final MessageComponent messageComponent;
+
+  public ExceptionAdvice(MessageComponent messageComponent) {
+    this.messageComponent = messageComponent;
+  }
+
+  @ExceptionHandler(InfoException.class)
+  public ViewContext handle(InfoException e) {
+    HtmxUtil.retarget(HtmxUtil.idSelector(LayoutComponent.TOAST_CONTAINER_ID));
+    HtmxUtil.swap(HxSwapType.INNER_HTML);
+    return messageComponent.renderInfoToast(e.getMessage());
+  }
+}
